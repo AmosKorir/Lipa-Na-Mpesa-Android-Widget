1e8c5cbc6159c33c5bf40bd8b215ede7913d3328
amoskorir <amoskrr@gmail.com>
2020-12-16 16:51:48 +0300

    ui re-design

--- lipanampesalib/src/main/java/com/lipanampesa/FloatingService.kt
+++ lipanampesalib/src/main/java/com/lipanampesa/FloatingService.kt
@@ -21,144 +21,152 @@ import androidx.core.view.isVisible
 import com.lipanampesa.R.string
 
 class FloatingService : Service(), View.OnClickListener {
-  lateinit var windowManager: WindowManager
-  lateinit var floatingBanner: View
-  lateinit var topLabel: Button
-  lateinit var collapseView: View
-  lateinit var minimizeButton: View
-  lateinit var accountView: View
-  lateinit var paybillView: View
-  lateinit var amountView: View
-  lateinit var accountTextView: TextView
-  lateinit var payBillTextView: TextView
-  lateinit var amountTextView: TextView
-  lateinit var closeView: ImageView
+    lateinit var windowManager: WindowManager
+    lateinit var floatingBanner: View
+    lateinit var topLabel: Button
+    lateinit var collapseView: View
+    lateinit var minimizeButton: View
+    lateinit var accountView: View
+    lateinit var paybillView: View
+    lateinit var amountView: View
+    lateinit var accountTextView: TextView
+    lateinit var payBillTextView: TextView
+    lateinit var amountTextView: TextView
+    lateinit var closeView: ImageView
+    lateinit var logoImageView: ImageView
 
-  private lateinit var lipaNaMpesa: LipaNaMpesa
+    private lateinit var lipaNaMpesa: LipaNaMpesa
 
-  override fun onBind(intent: Intent): IBinder? {
-    return null
-  }
-
-  override fun onClick(v: View?) {
-  }
-
-  override fun onStart(intent: Intent?, startId: Int) {
-    super.onStart(intent, startId)
-    val extras = intent!!.extras
-    if (extras == null) {
-      Log.d("Service", "lipa na mpesa param are empty")
-      throw (Throwable("Params required"))
-    } else {
-      lipaNaMpesa = extras.getSerializable(LipaNaMpesaConstants.LIPANAMPESA) as LipaNaMpesa
-      amountTextView.text = lipaNaMpesa.AMOUNT
-      accountTextView.text = lipaNaMpesa.ACCOUNT
-      payBillTextView.text = lipaNaMpesa.PAYBILL
-      setPrimaryColor(lipaNaMpesa.PRIMARYCOLOR)
-
-    }
-  }
-
-  private fun setPrimaryColor(primaryColor: Int) {
-    topLabel.setBackgroundColor(primaryColor)
-    closeView.setBackgroundColor(primaryColor)
-  }
-
-  override fun onCreate() {
-    super.onCreate()
-
-    //bind ui
-    floatingBanner = LayoutInflater.from(this).inflate(R.layout.mpesa_widget_layout, null)
-    collapseView = floatingBanner.findViewById(R.id.collapseView)
-    closeView = floatingBanner.findViewById(R.id.closingImage)
-    topLabel = floatingBanner.findViewById(R.id.topCollapsible)
-    minimizeButton = floatingBanner.findViewById(R.id.minimizeButtton)
-    accountView = floatingBanner.findViewById(R.id.accountView)
-    paybillView = floatingBanner.findViewById(R.id.payBillView)
-    payBillTextView = floatingBanner.findViewById(R.id.payBillNumber)
-    accountTextView = floatingBanner.findViewById(R.id.accountTxt)
-    amountView = floatingBanner.findViewById(R.id.amountView)
-    amountTextView = floatingBanner.findViewById(R.id.amount)
-
-    //layout params
-    val params = LayoutParams(
-      LayoutParams.WRAP_CONTENT,
-      LayoutParams.WRAP_CONTENT,
-      LayoutParams.TYPE_APPLICATION_OVERLAY,
-      LayoutParams.FLAG_NOT_FOCUSABLE,
-      PixelFormat.TRANSLUCENT
-    )
-    params.gravity = Gravity.TOP or Gravity.RIGHT
-    windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-    windowManager.addView(floatingBanner, params)
-
-    topLabel.setOnClickListener {
-      collapseExpand()
+    override fun onBind(intent: Intent): IBinder? {
+        return null
     }
 
-    closeView.setOnClickListener {
-      stopSelf()
+    override fun onClick(v: View?) {
     }
 
-
-
-    minimizeButton.setOnClickListener {
-      collapseLayout()
+    override fun onStart(intent: Intent?, startId: Int) {
+        super.onStart(intent, startId)
+        val extras = intent!!.extras
+        if (extras == null) {
+            Log.d("Service", "lipa na mpesa param are empty")
+            throw (Throwable("Params required"))
+        } else {
+            lipaNaMpesa = extras.getSerializable(LipaNaMpesaConstants.LIPANAMPESA) as LipaNaMpesa
+            amountTextView.text = lipaNaMpesa.AMOUNT
+            accountTextView.text = lipaNaMpesa.ACCOUNT
+            payBillTextView.text = lipaNaMpesa.PAYBILL
+        }
     }
 
-    //copy data
+    override fun onCreate() {
+        super.onCreate()
 
-    paybillView.setOnClickListener {
-      copyText(payBillTextView)
+        //bind ui
+        floatingBanner = LayoutInflater.from(this).inflate(R.layout.mpesa_widget_layout, null)
+        collapseView = floatingBanner.findViewById(R.id.collapseView)
+        closeView = floatingBanner.findViewById(R.id.closingImage)
+        topLabel = floatingBanner.findViewById(R.id.topCollapsible)
+        minimizeButton = floatingBanner.findViewById(R.id.minimizeButtton)
+        accountView = floatingBanner.findViewById(R.id.accountView)
+        paybillView = floatingBanner.findViewById(R.id.payBillView)
+        payBillTextView = floatingBanner.findViewById(R.id.payBillNumber)
+        accountTextView = floatingBanner.findViewById(R.id.accountTxt)
+        amountView = floatingBanner.findViewById(R.id.amountView)
+        amountTextView = floatingBanner.findViewById(R.id.amount)
+        logoImageView = floatingBanner.findViewById(R.id.logoImg)
+
+        //layout params
+        val params = LayoutParams(
+            LayoutParams.WRAP_CONTENT,
+            LayoutParams.WRAP_CONTENT,
+            LayoutParams.TYPE_APPLICATION_OVERLAY,
+            LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSLUCENT
+        )
+        params.gravity = Gravity.TOP or Gravity.RIGHT
+        windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        windowManager.addView(floatingBanner, params)
+
+        topLabel.setOnClickListener {
+            collapseExpand()
+        }
+
+        closeView.setOnClickListener {
+            stopSelf()
+        }
+
+        logoImageView.setOnClickListener {
+            collapseExpandPayment()
+        }
+
+        minimizeButton.setOnClickListener {
+            collapseLayout()
+            topLabel.setText(string.click_to_expand)
+        }
+
+        //copy data
+
+        paybillView.setOnClickListener {
+            copyText(payBillTextView)
+        }
+        accountView.setOnClickListener {
+            copyText(accountTextView)
+        }
+        amountView.setOnClickListener {
+            copyText(amountTextView)
+        }
+
+
     }
-    accountView.setOnClickListener {
-      copyText(accountTextView)
-    }
-    amountView.setOnClickListener {
-      copyText(amountTextView)
+
+    private fun collapseLayout() {
+        collapseView.visibility = View.GONE
     }
 
-  }
-
-  fun collapseLayout() {
-//    topExpand.visibility = View.VISIBLE
-    collapseView.visibility = View.GONE
-  }
-
-  fun expandLayout() {
-    collapseView.visibility = View.VISIBLE
-//    topExpand.visibility = View.GONE
-
-  }
-
-  fun copyText(view: View?) {
-    lateinit var clipboardManager: ClipboardManager
-
-    val text: String = (view as TextView).text.toString()
-    if (text.isNotEmpty()) {
-      clipboardManager =
-        getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-      val clipData = ClipData.newPlainText("lipaKey", text)
-      clipboardManager.setPrimaryClip(clipData)
-      Toast.makeText(applicationContext, "Copied", Toast.LENGTH_SHORT).show()
-    } else {
-      Toast.makeText(applicationContext, "No text to be copied", Toast.LENGTH_SHORT).show()
+    private fun expandLayout() {
+        collapseView.visibility = View.VISIBLE
     }
-  }
 
-  fun collapseExpand() {
-    if (collapseView.isVisible) {
-      collapseLayout()
-      topLabel.setText(string.click_to_expand)
-    } else {
-      expandLayout()
-      topLabel.text = getString(string.payment_details)
+    private fun copyText(view: View?) {
+        lateinit var clipboardManager: ClipboardManager
+
+        val text: String = (view as TextView).text.toString()
+        if (text.isNotEmpty()) {
+            clipboardManager =
+                getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clipData = ClipData.newPlainText("lipaKey", text)
+            clipboardManager.setPrimaryClip(clipData)
+            Toast.makeText(applicationContext, getString(string.copied), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, getString(string.no_text), Toast.LENGTH_SHORT).show()
+        }
     }
-  }
 
-  override fun onDestroy() {
-    super.onDestroy()
-    windowManager.removeView(floatingBanner)
-  }
+    private fun collapseExpand() {
+        if (collapseView.isVisible) {
+            collapseLayout()
+            topLabel.setText(string.click_to_expand)
+        } else {
+            expandLayout()
+            topLabel.text = getString(string.payment_details)
+        }
+    }
+
+    private fun collapseExpandPayment() {
+        if (collapseView.isVisible) {
+            collapseLayout()
+            topLabel.visibility = View.GONE
+            closeView.visibility = View.GONE
+        } else {
+            topLabel.visibility = View.VISIBLE
+            closeView.visibility = View.VISIBLE
+            expandLayout()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        windowManager.removeView(floatingBanner)
+    }
 
 }
